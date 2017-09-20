a8f227f38b7b86f0d45e37ef505407871b62794c
parnapiano <paran_piano@hotmail.com>
2017-09-20 16:31:52 +0900

    commit first create Int, string class

--- GenericPractice/Program.cs
+++ GenericPractice/Program.cs
@@ -10,6 +10,53 @@ namespace GenericPractice
     {
         static void Main(string[] args)
         {
+            var intNumbers = new MyIntList();
+            intNumbers.AddInt(10);
+            intNumbers.AddInt(20);
+            intNumbers.AddInt(30);
+            intNumbers.AddInt(40);
+            intNumbers.AddInt(50);
+
+            Console.WriteLine(intNumbers.getNumber(2));
+
+            var stringNumbers = new MyStringList();
+            stringNumbers.AddInt("A");
+            stringNumbers.AddInt("B");
+            stringNumbers.AddInt("C");
+            stringNumbers.AddInt("D");
+            stringNumbers.AddInt("E");
+
+            Console.WriteLine(stringNumbers.getString(2));
+        }
+    }
+
+    public class MyIntList
+    {
+        private List<int> mNumbers = new List<int>();
+
+        public void AddInt(int number)
+        {
+            mNumbers.Add(number);
+        }
+
+        public int getNumber(int i)
+        {
+            return mNumbers[i];
+        }
+    }
+
+    public class MyStringList
+    {
+        private List<string> mStrings = new List<string>();
+
+        public void AddInt(string str)
+        {
+            mStrings.Add(str);
+        }
+
+        public string getString(int i)
+        {
+            return mStrings[i];
         }
     }
 }
